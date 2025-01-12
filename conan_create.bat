abcde3f6800c3f6d113c25ad2371b77b1e3cb3b9
fajkoson <57269743+fajkoson@users.noreply.github.com>
2025-01-12 21:27:49 +0100

    winflexbison - conan

---
+++ conan_create.bat
@@ -0,0 +1,3 @@
+.venv\Scripts\conan create . --version=2.5.24 --user=tescan --channel=stable
+:: REQUIRES predownload of the patch and put it into folder patches
+REM .venv\Scripts\conan create . --version=1.9.4
